5d1ce932d1bf1007ac699ec5b0aa49457c1436fb
Katie Rose <katiefelten@gmail.com>
2019-05-10 11:24:05 -0400

    Updated objects-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,47 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = { 
+	diet: "carnivorous", 
+	weight: '7000kg', 
+  length: '12m',
+  period: "Late Cretaceous",
+  roar: "RAWERSRARARWERSARARARRRR!"
+}  
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = { 
+	diet: "herbivorous", 
+	weight: '2000kg', 
+  length: '9m',
+  period: "Late Jurassic"
+} 
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor = { 
+	diet: "carnivorous", 
+	weight: '15kg', 
+  length: '1.8m',
+  period: "Late Cretaceous"
+} 
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar);
 
 
 // ==== Arrays ====
@@ -50,7 +69,16 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+let univeristies = graduates.sort(function(schoolA, schoolB) {
+  if (schoolA.university < schoolB.university) {
+    return -1;
+  }
+  return 1;
+});
+
+for (let i = 0; i < univeristies.length; i++) {
+  console.log(univeristies[i].university);
+}
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
